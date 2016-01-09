9fd4ef63f4993c2b57fdb8d24c8ab023766c9e81
Nathan <nathanlewis42@gmail.com>
2016-01-09 20:07:43 +0000

    latest work in progress

--- src/main/java/uk/org/interzone/Main.java
+++ src/main/java/uk/org/interzone/Main.java
@@ -1,7 +1,17 @@
 package uk.org.interzone;
 
+import org.imgscalr.Scalr;
+import org.imgscalr.Scalr.*;
+import java.awt.image.*;
+import java.io.*;
+import javax.imageio.*;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FilenameFilter;
+import java.io.IOException;
+
+import static org.imgscalr.Scalr.resize;
 
 /**
  * Hello world!
@@ -22,8 +32,29 @@ public class Main
 
         for (File picfile : files) {
             System.out.println(picfile);
+            System.out.println(picfile.toString());
+            //            try {
+//                res(picfile.getName(),"../photos");
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
         }
 
+    }
+
+    public static void res(String sourceFilename, String dirname) throws IOException {
+
+        File sourceImageFile = new File(sourceFilename);
+        File outputFile = new File("thumbnails/" + sourceImageFile.getName());
+        if (outputFile.exists()) {
+            return;
+        }
+        BufferedImage img = ImageIO.read(sourceImageFile);
+
+        BufferedImage thumbnail = resize(img, 500);
+
+        thumbnail.createGraphics().drawImage(thumbnail, 0, 0, null);
+        ImageIO.write(thumbnail, "jpg", outputFile);
     }
 
 }
